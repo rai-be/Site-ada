abb70696b73b34f02bfb8ce2ee411b0901a287fa
Raiani Silva <105001798+rai-be@users.noreply.github.com>
2023-03-05 17:36:20 +0000

    listener

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+  alert ("Prontinho! voce recebera as novidades por email")
+ }else{
+   alert("peça a usuária que preencha os campos nome e email")
+ }
