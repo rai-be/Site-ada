f235f6fa71f0e8bf49452b503cb7bcb13a3af4d9
Raiani Silva <105001798+rai-be@users.noreply.github.com>
2023-03-07 22:02:06 +0000

    function javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
-  alert ("Prontinho! voce recebera as novidades por email")
- }else{
-   alert("peça a usuária que preencha os campos nome e email")
- }
+document.getElementById("botaoenviar").addEventListener("click"
+, validaformulario)
+
+function validaformulario (){
+if(document.getElementById("nome").value != "" &&
+      document.getElementById("email").value != "" &&
+      document.getElementById("telefone").value != "" ){
+
+    alert("Prontinho! você receberá as novidades por email.")
+   }else{
+     alert("Por favor, preencha os campos nome e email.")
+   } 
+}
